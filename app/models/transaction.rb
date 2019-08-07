7db5f25bdfa441347b5473b7c735c3722202bcce
MattPoetker <matt.pworks@gmail.com>
2019-08-07 09:17:02 -0500

    Added tests for transaction validation

--- app/models/transaction.rb
+++ app/models/transaction.rb
@@ -1,3 +1,12 @@
 class Transaction < ApplicationRecord
-  
+  validates :date, presence: true
+  validate :is_valid_date
+
+  private
+
+  def is_valid_date
+    if date == nil
+      self.errors[:base] << "Date format is invalid"
+    end
+  end
 end
--- spec/models/transaction_spec.rb
+++ spec/models/transaction_spec.rb
@@ -1,7 +1,16 @@
 require 'rails_helper'
 
 RSpec.describe Transaction, type: :model do
-  describe "A transaction" do
-    it "is a transaction!"
+
+  #
+  describe "on saving" do
+    it "should be valid with valid attributes" do
+      transaction = Transaction.new(date: DateTime.now.to_date)
+      expect(transaction).to be_valid
+    end
+    it "should be invalid without a transaction date" do
+      transaction = Transaction.new
+      expect(transaction).to_not be_valid
+    end
   end
 end
