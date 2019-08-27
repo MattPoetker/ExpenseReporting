83bf0329acd590ccc4ed90208074dcfe5344b97b
Matthew Poetker <matt.pworks@gmail.com>
2019-08-27 16:50:11 -0500

    More changes for Heroku

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,7 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   helper_method :current_org
+  before_action :set_current_org
   def current_org
     @current_org = current_user.organization
   end
@@ -8,7 +9,10 @@ class ApplicationController < ActionController::Base
   private
 
   protected
-  
+  def set_current_org
+    @current_org = current_user.organization
+  end
+
   def after_sign_in_path_for(resource)
     '/dashboard'
   end
