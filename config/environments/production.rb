1b47b34a1e50e0bea42f2dc44279adaa0c4ba2f0
Matthew Poetker <matt.pworks@gmail.com>
2019-09-18 16:30:55 -0500

    More changes for SendGrid

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,6 +1,7 @@
 Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
   config.action_mailer.delivery_method = :smtp
+  config.action_mailer.default_url_options = { :host => 'expense-reporting.herokuapp.com' }
   config.action_mailer.perform_deliveries = true
   config.action_mailer.smtp_settings = {
     :user_name => ENV['SENDGRID_USERNAME'],
