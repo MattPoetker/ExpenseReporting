9ec6482ccad3e735ac9c2926d7029db42135f9f9
MattPoetker <matt.pworks@gmail.com>
2019-09-18 10:58:14 -0500

    modifications to the home view and controller

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,13 +1,22 @@
 class HomeController < ApplicationController
   before_action :set_current_org
+  helper_method :user_is_admin?
   def index
-    @transactions = Transaction.all
+    if user_is_admin?
+      @transactions = current_user.organization.transactions
+    else
+      @transactions = current_user.transactions
+    end
     @users = User.all
-    @admin = Admin.all
     @organizations = Organization.all
   end
 
   private
+  def user_is_admin?
+    if current_user
+      current_user.admin
+    end
+  end
   def set_current_org
     if current_user
       @current_org = current_user.organization
