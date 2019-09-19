880d8f9f344d4a5a12e31ad45916d9b16750ba50
Matthew Poetker <matt.pworks@gmail.com>
2019-09-18 21:12:37 -0500

    improved templates and added a helper method for displaying transaction statuses

--- app/helpers/transactions_helper.rb
+++ app/helpers/transactions_helper.rb
@@ -1,2 +1,11 @@
 module TransactionsHelper
+  def transaction_status(transaction)
+    if transaction.transaction_status == "Submitted" || transaction.transaction_status == "On Hold"
+      "badge-warning"
+    elsif transaction.transaction_status == "Verified"
+      "badge-primary"
+    elsif transaction.transaction_status == "Paid"
+      "badge-success"
+    end
+  end
 end
